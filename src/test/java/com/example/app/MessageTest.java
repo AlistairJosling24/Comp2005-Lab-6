aae7c997c19220ea69e6b7e69ff7e252609560e2
alist <alistairjosling@outlook.com>
2026-03-23 19:02:10 +0000

    Initial commit

---
+++ src/test/java/com/example/app/MessageTest.java
@@ -0,0 +1,18 @@
+package com.example.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageTest {
+
+    @Test
+    void getMessage() {
+        String expected = "Hello there!";
+
+        String actual = Message.getMessage();
+
+        assertEquals(actual, expected);
+    }
+}
